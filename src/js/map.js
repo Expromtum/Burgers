7f50bc48d61f1f9c3de47aa32278a456fe4a2996
Marina Sokurenko <stagger@mail.ru>
2018-02-05 15:58:57 +0300

    Секция Контакты: Маркер на карте

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,23 @@
 var map;
 
 function initMap() {
+
+	var locations = {lat: 55.687085, lng: 37.562749};
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 16
+        center: locations,
+        zoom: 17
     });
+
+	//var markerImage = '../icons/map-marker.png';
+
+	var markerImage = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+	
+	var marker = new google.maps.Marker({
+		position: locations,
+		map: map,
+		icon: markerImage
+	});
+
 }
 
